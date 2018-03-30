21224a148751be469a49845f5d44adb7a19c0e93
skynetv <zjtopspeed@gmail.com>
2018-03-30 18:24:49 +0800

    init

---
+++ CoreSpider/CoreSpider/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Http;
+
+namespace CoreSpider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
